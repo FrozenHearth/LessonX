152237d3c82976190645e901718a2cf366238c81
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-01 18:02:28 +0530

    Added content and styling to left menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react';
+import '../styles/Sidebar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCheckCircle,
+  faTimesCircle,
+  faEdit
+} from '@fortawesome/free-solid-svg-icons/';
 
 export default class SideBar extends Component {
   render() {
     return (
-      <div>
-        <div>This is the sidebar.</div>
-      </div>
+      <section className="sidebar-container">
+        <div className="menu-top">
+          <h2 className="menu-content-title">Menu</h2>
+        </div>
+        <div className="menu-bottom">
+          <div className="links-container">
+            <a href="#/">Video Link 1</a>
+            <a href="#/">Video Link 2</a>
+            <a href="#/">Video Link 3</a>
+          </div>
+          <div className="objectives-container">
+            <div className="objectives">
+              <h2 className="objective-title">Time Signature 3/4</h2>
+              <span className="objective-duration">(15 Mins)</span>
+              <span className="icon-container">
+                <a href="#/" className="check-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faCheckCircle} />
+                </a>
+                <a href="#/" className="cross-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faTimesCircle} />
+                </a>
+                <a href="#/" className="edit-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faEdit} />
+                </a>
+              </span>
+            </div>
+
+            <div className="objectives">
+              <h2 className="objective-title">C5 Finger Scale RH</h2>
+              <span className="objective-duration">(15 Mins)</span>
+              <span className="icon-container">
+                <a href="#/" className="check-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faCheckCircle} />
+                </a>
+                <a href="#/" className="cross-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faTimesCircle} />
+                </a>
+                <a href="#/" className="edit-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faEdit} />
+                </a>
+              </span>
+            </div>
+
+            <div className="objectives">
+              <h2 className="objective-title">E5 Finger Scale LH</h2>
+              <span className="objective-duration">(15 Mins)</span>
+              <span className="icon-container">
+                <a href="#/" className="check-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faCheckCircle} />
+                </a>
+                <a href="#/" className="cross-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faTimesCircle} />
+                </a>
+                <a href="#/" className="edit-icon">
+                  <FontAwesomeIcon className="fa-lg" icon={faEdit} />
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }

4d0780d3bb940ea56decf009b5449a715f57850e
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-01 15:05:56 +0530

    Created the necessary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 import './App.css';
+import Header from './components/Header';
+import SideBar from './components/Sidebar';
+import VideoContent from './components/VideoContent';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <SideBar />
+      <VideoContent />
+    </div>
+  );
+};
 
 export default App;

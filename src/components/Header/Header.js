21a04fb0987c097d3e66523e4856ed8b1fac9f44
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-02 12:36:18 +0530

    Added styling for active lesson, and showed lesson objectives on click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,15 @@ import Lessons from './Lessons';
 
 export default class Header extends Component {
   render() {
+    const { recitalTitle, instrumentTitle, lessonDetails } = this.props;
     return (
       <>
         <div className="header-top-container">
           <img src={ConductorIcon} className="conductor-icon" alt="" />
-          <h1 className="recital-title">Lavender's Blue</h1>
-          <h2 className="instrument-title">Guitar</h2>
+          <h1 className="recital-title">{recitalTitle}</h1>
+          <h2 className="instrument-title">{instrumentTitle}</h2>
         </div>
-        <Lessons />
+        <Lessons lessonDetails={lessonDetails} />
       </>
     );
   }
--- src/components/Header/Lessons.js
+++ src/components/Header/Lessons.js
@@ -1,38 +1,59 @@
 import React, { Component } from 'react';
 import '../../styles/Lessons.css';
+import SideBar from '../Sidebar';
+import VideoContent from '../VideoContent';
 
 export default class Lessons extends Component {
+  state = {
+    objectiveDetails: [],
+    clickedLessonId: ''
+  };
+  getClickedLessonNumber = lesson => {
+    console.log(lesson);
+
+    this.setState({
+      objectiveDetails: lesson.objectiveDetails,
+      clickedLessonId: lesson.id
+    });
+  };
   render() {
+    const { lessonDetails } = this.props;
+    const { objectiveDetails, clickedLessonId } = this.state;
+    // console.log(this.props.lessonDetails);
     return (
-      <div className="header-bottom-container">
-        <div className="lessons-container">
-          <h2 className="lesson-header">Lessons</h2>
-          <div className="lesson-numbers-container">
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">1</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">2</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">3</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">4</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">5</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">6</span>
-            </span>
-            <span className="lesson-numbers-circle">
-              <span className="lesson-numbers">7</span>
-            </span>
+      <>
+        <div className="header-bottom-container">
+          <div className="lessons-container">
+            <h2 className="lesson-header">Lessons</h2>
+            <div className="lesson-numbers-container">
+              {lessonDetails
+                ? lessonDetails.map(lesson => (
+                    <span
+                      onClick={() => this.getClickedLessonNumber(lesson)}
+                      key={lesson.id}
+                      className="lesson-numbers-circle"
+                      style={
+                        clickedLessonId !== lesson.id
+                          ? { background: '#ff9811' }
+                          : { background: '#fff' }
+                      }
+                    >
+                      <span className="lesson-numbers">
+                        {lesson.lessonTitle.replace('Lesson', '')}
+                        {/* Remove the word 'Lesson' from the lesson Title & only show Lesson number */}
+                      </span>
+                    </span>
+                  ))
+                : ''}
+            </div>
+            <button className="exit-session-btn">Exit Session</button>
           </div>
-          <button className="exit-session-btn">Exit Session</button>
         </div>
-      </div>
+        <div className="hero-container">
+          <SideBar objectiveDetails={objectiveDetails} />
+          <VideoContent />
+        </div>
+      </>
     );
   }
 }

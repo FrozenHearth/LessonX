3ad4b75829d3ba90c88adabd7cf8b2a7522c3016
Vishu <brs@Vishus-MacBook-Air.local>
2020-04-02 18:50:44 +0530

    Added video embedding, onClick video display for objectives

--- src/components/VideoContent.js
+++ src/components/VideoContent.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player';
 import '../styles/Videos.css';
+import PlayIcon from './utils/PlayIcon';
 
 export default class VideoContent extends Component {
+  constructor(props) {
+    super(props);
+    this.reactPlayerRef = React.createRef();
+  }
+
   render() {
+    const { objectiveVideosDetails } = this.props;
+    const { backgroundImg } = this.state;
     return (
-      <div className="video-container">
+      <div
+        style={{ background: `url(${backgroundImg})` }}
+        className="video-container"
+      >
         <div className="video-thumbnails-container">
-          <div className="thumbnails"></div>
-          <div className="thumbnails"></div>
-          <div className="thumbnails"></div>
+          {objectiveVideosDetails
+            ? objectiveVideosDetails.map(video => (
+                <div className="thumbnails" key={video.id}>
+                  <ReactPlayer
+                    ref={this.reactPlayerRef}
+                    width="100%"
+                    height="100%"
+                    light={true}
+                    playIcon={<PlayIcon />}
+                    controls={true}
+                    url={video.url}
+                  />
+                </div>
+              ))
+            : null}
         </div>
       </div>
     );
